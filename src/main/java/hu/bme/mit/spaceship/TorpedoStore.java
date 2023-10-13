a7a786c968c45ad4121b9dfa89d45bbcad5e2e3b
unknown <175890@BME.HU>
2023-10-13 13:38:24 +0200

    Fix for Nandi1218

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -14,7 +14,7 @@ public class TorpedoStore {
 
   private int torpedoCount = 0;
 
-  private Random generator = new Random();
+  private Random generator = new Random(); // Nandi1218 best reviewer
 
 
   public TorpedoStore(int numberOfTorpedos){
